04c075f0344ffe2616c55206af104079fb01c013
unknown <dcahyo1994@gmail.com>
2015-04-08 12:55:29 +0700

    kuliah 4

--- includes/cmsApplication.php
+++ includes/cmsApplication.php
@@ -1,12 +1,29 @@
 <?php 
 require_once('cmsBase.php');
 class CmsApplication extends CmsBase {
+	
+	function run() {
+		// di sini akan dibuat kode,
+		// untuk mengatur seluruh proses dalam cmsApplication.
+		// Fungsi ini akan dipanggil dari modul TemplateFunction		
+		if(isset($_REQUEST['task'])){
+			$task=$_REQUEST['task'];
+			switch($task){
+				case 'addcontent':$app->addcontent();break;
+				case 'anyothertask':$app->anyothertask();break;
+				default: $app->display();break;
+			}    
+		}else{
+			$this->display();
+		}
+	}
+	
 	function addcontent ()
 	{
 		echo 'Di sini akan dibuat fungsi menambah konten';
 	}
 	
-	function viewcontent ()
+	function display ()
 	{
 		echo 'Di sini akan dibuat fungsi untuk menampilkan konten';
 	}
@@ -14,6 +31,11 @@ class CmsApplication extends CmsBase {
 	function anyothertask ()
 	{
 		echo 'Di sini akan ditulis fungsi yang lainnya';
+	}
+	
+	function show()
+	{
+		require_once($this->getCurrentTemplatePath().'template.php');
 	}
 	
 }
--- includes/templateFunctions.php
+++ includes/templateFunctions.php
@@ -1,6 +1,27 @@
 <?php 
 require_once('cmsBase.php');
 class TemplateFunctions extends CmsBase {
-	// Semua fungsi terkait dengan
-	// pengaturan template ada di sini
+var $templateName='default';
+
+function show()
+{
+require_once($this->getCurrentTemplatePath().'template.php');
 }
+
+function getCurrentTemplatePath()
+{
+return 'templates/'.$this->templateName.'/';
+}
+
+function setTemplate($templateName)
+{
+$this->templateName = $templateName;
+}
+
+function appOutput(){
+require_once('includes/cmsApplication.php');
+$app=new CmsApplication();
+$app->run();
+}
+}
+?>
