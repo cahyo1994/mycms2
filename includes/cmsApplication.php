cc8674fdcaad9c64835b2075320052629b437ecd
unknown <dcahyo1994@gmail.com>
2015-04-08 12:06:46 +0700

    Menambah file template dan css

--- includes/cmsApplication.php
+++ includes/cmsApplication.php
@@ -1,9 +1,19 @@
 <?php 
 require_once('cmsBase.php');
-//memastikan modul cmsBase hanya di lOAD sekali
 class CmsApplication extends CmsBase {
-	// semua kode yang ada di sini 
-	// dapat diakses melalui fungsi utama
-	// dalam CMS 
-	// management halaman web
+	function addcontent ()
+	{
+		echo 'Di sini akan dibuat fungsi menambah konten';
+	}
+	
+	function viewcontent ()
+	{
+		echo 'Di sini akan dibuat fungsi untuk menampilkan konten';
+	}
+	
+	function anyothertask ()
+	{
+		echo 'Di sini akan ditulis fungsi yang lainnya';
+	}
+	
 }
